d6bf9c28dbae6256072a74a6890b4248a8637b4f
github-action-benchmark <github@users.noreply.github.com>
2026-07-04 02:24:34 +0000

    add Piano Performance (macOS) (customBiggerIsBetter) benchmark result for 46d25217073d03745c5dfef617967e5b47ec05f7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1783131499233,
+  "lastUpdate": 1783131874123,
   "repoUrl": "https://github.com/JG8203/Piano",
   "entries": {
     "Piano Performance (Windows)": [
@@ -2817,6 +2817,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "16 voices",
             "value": 27.2786,
+            "unit": "x realtime"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joshua_gilo@dlsu.edu.ph",
+            "name": "Joshua Armaine Gilo",
+            "username": "JG8203"
+          },
+          "committer": {
+            "email": "joshua_gilo@dlsu.edu.ph",
+            "name": "Joshua Armaine Gilo",
+            "username": "JG8203"
+          },
+          "distinct": true,
+          "id": "46d25217073d03745c5dfef617967e5b47ec05f7",
+          "message": "Document pkg-config Linux dependency",
+          "timestamp": "2026-07-04T10:20:59+08:00",
+          "tree_id": "7f8c69b14a32d8418c99c8842a3287cfc22c286c",
+          "url": "https://github.com/JG8203/Piano/commit/46d25217073d03745c5dfef617967e5b47ec05f7"
+        },
+        "date": 1783131872401,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "1 voice",
+            "value": 134.6939,
+            "unit": "x realtime"
+          },
+          {
+            "name": "4 voices",
+            "value": 64.17,
+            "unit": "x realtime"
+          },
+          {
+            "name": "8 voices",
+            "value": 48.7663,
+            "unit": "x realtime"
+          },
+          {
+            "name": "16 voices",
+            "value": 32.1385,
             "unit": "x realtime"
           }
         ]
